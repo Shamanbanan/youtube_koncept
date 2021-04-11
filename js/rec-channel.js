e8c4452e593bca7c12550eab92862d5e60ccfcb8
Shamanbanan <72462143+Shamanbanan@users.noreply.github.com>
2021-04-11 23:44:36 +0300

    доделал карскас полностью

---
+++ js/rec-channel.js
@@ -0,0 +1,13 @@
+const recomm = new Swiper('.group-channel', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 6,
+ 
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.group-button-next',
+      prevEl: '.group-button-prev',
+      
+    },
+  });
---
+++ js/recommended.js
@@ -0,0 +1,14 @@
+const recommend = new Swiper('.channel-recommend', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 3,
+ 
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.recommend-button-next',
+      prevEl: '.recommend-button-prev',
+      
+    },
+  });
+  
