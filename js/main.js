a54be897869c5f9184dd46242ff007764133125d
Shamanbanan <72462143+Shamanbanan@users.noreply.github.com>
2021-04-16 00:20:38 +0300

    адаптив блоков

--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
 const swiper = new Swiper('.channel-slider', {
     // Optional parameters
     loop: true,
-    slidesPerView: 6,
- 
-  
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+      1900:{
+        slidesPerView: 6
+      },
+      1600:{
+        slidesPerView: 5
+      },
+      1300:{
+        slidesPerView: 4
+      },
+      1100:{
+        slidesPerView: 3
+      },
+      800:{
+        slidesPerView: 2
+      }
+    },
     // Navigation arrows
     navigation: {
       nextEl: '.channel-button-next',
@@ -12,3 +28,37 @@ const swiper = new Swiper('.channel-slider', {
     },
   });
 
+const recommend = new Swiper('.channel-recommend', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+      1600:{
+        slidesPerView: 3
+      },
+      1110:{
+        slidesPerView: 2
+      }
+    },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.recommend-button-next',
+      prevEl: '.recommend-button-prev',
+      
+    },
+  });
+
+const recomm = new Swiper('.group-channel', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 6,
+ 
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.group-button-next',
+      prevEl: '.group-button-prev',
+      
+    },
+  });
